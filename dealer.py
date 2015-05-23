c965016c666fa8f566e77f76961774c3d6de2930
Alex Wenzel <alexanderwenzel2017@u.northwestern.edu>
2015-05-23 18:53:44 -0500

    player and things

--- dealer.py
+++ dealer.py
@@ -3,25 +3,52 @@ from Card import *
 from bjutils import *
 from player import *
 
+##info to send to decider##
+#ace or not
+#dealer's face card
+#cards played (len deck)
+#value of hand(s)
+
+
 class Dealer:
-	def __init__():
+	def __init__(self):
 		self.deck = Deck()
 		self.hand = []
 		self.hole = None
 
-	def dealerDraw():
+	def dealerDraw(self):
 		self.hand.append(self.hole)
-		while bjutils.valHand(self.hand) < 17:
-			self.hand.append(self.deck.Pop(0))
+		while valHand(self.hand) < 17:
+			self.hand.append(self.deck.cards.Pop(0))
 	
-	def deal(players):
-		for p in players:
-			p.hand.append(self.deck.Pop(0))
-			p.hand.append(self.deck.Pop(0))
-		self.hand.append(self.deck.Pop(0))
-		self.hole = self.deck.Pop(0)
+	def deal(self,p):
+		p.hand.append(self.deck.cards.pop(0))
+		p.hand.append(self.deck.cards.pop(0))
+		for card in p.hand:
+			p.seen.append(card)
+		self.hand.append(self.deck.cards.pop(0))
+		p.seen.append(self.hand[0])
+		self.hole = self.deck.cards.pop(0)
 
-	def playRound(players):
-		for p in players:
-			pmove = p.decide()
-			if pmove == 
+	def playRound(self, p):
+		#dec = p.decide()
+		dec = "HIT"
+		if dec == "HIT":
+			p.hand.append(self.deck.cards.pop(0))
+			p.seen.append(p.hand[-1])
+			#print(type(valHand(p.hand)))
+			if valHand(p.hand) > 21:
+				return -1
+		elif dec == "STAND":
+			self.dealerDraw()
+			if valHand(self.hand) > 21:
+				return 1
+			elif valHand(p.hand) > 21:
+				return -1
+			if valHand(self.hand) > bjutils.valHand(p.hand):
+				return -1
+			elif valHand(self.hand) < bjutils.valHand(p.hand):
+				return 1
+			elif valHand(self.hand) == bjutils.valHand(p.hand):
+				return 0
+		return 2		
