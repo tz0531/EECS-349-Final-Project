7f6200651e88296cd09961c222eb56f788e42cc1
Alex Wenzel <alexanderwenzel2017@u.northwestern.edu>
2015-05-23 18:53:17 -0500

    player

--- player.py
+++ player.py
@@ -4,18 +4,19 @@ from dealer import *
 from Card import *
 
 class Player:
-	def __init__():
+	def __init__(self):
 		self.hand = []
+		self.seen = []
 
-        def hit(dealer):
-                self.hand.append(dealer.deck.Pop(0))
-
-        def canSplit():
+        #def hit(dealer):
+                #self.hand.append(dealer.deck.Pop(0))
+	##Implemented this above function in dealer.py
+        def canSplit(self):
                 if len(self.hand) == 2 and self.hand[0].value == self.hand[1].value:
                         return True
                 else:
                         return False
 	
-	def decide():
+	def decide(self):
 		#player decision code goes here
 		return 0
